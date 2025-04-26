41f47cb00cd0a9de00e02a3c571ab744648671a1
Jhhhadev <jhaded777@gmail.com>
2025-04-26 01:49:05 -0300

    Minha atualização feita agora

--- MauiAppMeuComb/MainPage.xaml.cs
+++ MauiAppMeuComb/MainPage.xaml.cs
@@ -17,13 +17,15 @@
                 double gasolina = Convert.ToDouble(txt_gasolina.Text);
 
                 string msg = "";
+                string marca = txt_marca.Text;
+                string modelo = txt_modelo.Text;             ;
 
                 if (etanol <= (gasolina * 0.7))
                 {
-                    msg = "O etanol está compensando";
+                    msg = $"O etanol está compensando para o seu {marca} {modelo}."; //interpolação de strings 
                 }else
                 {
-                    msg = "A gasolina está compensando";
+                    msg = "A Gasolina está compensando" + marca + " " + modelo + "."; //concatenação de strings
                 }
 
                 DisplayAlert("Calculado", msg, "OK");
