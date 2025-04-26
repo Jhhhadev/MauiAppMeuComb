7ef2c590bd55af430873c39e1616e3b83a03a855
jhade <jhade@DESKTOP-07UKV8I>
2025-04-26 00:38:25 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMeuComb/MainPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿namespace MauiAppMeuComb
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+
+            try
+            {
+                double etanol = Convert.ToDouble(txt_etanol.Text);
+                double gasolina = Convert.ToDouble(txt_gasolina.Text);
+
+                string msg = "";
+
+                if (etanol <= (gasolina * 0.7))
+                {
+                    msg = "O etanol está compensando";
+                }else
+                {
+                    msg = "A gasolina está compensando";
+                }
+
+                DisplayAlert("Calculado", msg, "OK");
+
+
+            }catch (Exception ex)
+            { //captura uma excessão, caso de algum erro o usuario cai nessa estrutura e exibo uma msg na tela
+                DisplayAlert("Ops", ex.Message, "Fechar");
+            }
+
+
+
+        } //fecha método
+    }// fecha class
+
+}//fecha namespace
---
+++ MauiAppMeuComb/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiAppMeuComb
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
